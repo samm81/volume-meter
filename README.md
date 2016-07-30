74b70aa7ba5f6d97b8faae0146b9659f01638855
samm81 <samwmaynard@gmail.com>
2016-07-29 23:06:00 -0700

    Ring a bell if it gets too loud

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# Simple volume meter
+# Shut Up Students
 
-I whipped this app up to show a basic volume meter on live audio input.  It does both clip detection and RMS volume.
-
-A "volume" meter can mean many things; if you want to do clip detection, you really need to access every sample.  If you don't need clip detection, I might suggest using an Analyser and getByteTimeDomainData, since it will likely have lower CPU overhead.  Note that it is CRITICALLY IMPORTANT to disassociate visual rendering (in the requestAnimationFrame loop) from the onaudioprocess function - you do NOT want to trigger a relayout from inside your audio handler, or it may glitch or cause other issues.
-
-It's also hosted at http://webaudiodemos.appspot.com/volume-meter/.
-
-Check it out, feel free to fork, submit pull requests, etc.  MIT-Licensed - party on.
-
--Chris
+Ring a bell if volume gets too loud! Maybe useful for classroom noise level control.
--- main.js
+++ main.js
@@ -28,76 +28,87 @@ var WIDTH=500;
 var HEIGHT=50;
 var rafID = null;
 
+var bell = new Audio('bell.mp3');
+var bellNotInterfering = true;
+
 window.onload = function() {
 
-    // grab our canvas
-	canvasContext = document.getElementById( "meter" ).getContext("2d");
-	
-    // monkeypatch Web Audio
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-	
-    // grab an audio context
-    audioContext = new AudioContext();
+	// grab our canvas
+	canvasContext = document.getElementById("meter").getContext("2d");
 
-    // Attempt to get audio input
-    try {
-        // monkeypatch getUserMedia
-        navigator.getUserMedia = 
-        	navigator.getUserMedia ||
-        	navigator.webkitGetUserMedia ||
-        	navigator.mozGetUserMedia;
+	// monkeypatch Web Audio
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-        // ask for an audio input
-        navigator.getUserMedia(
-        {
-            "audio": {
-                "mandatory": {
-                    "googEchoCancellation": "false",
-                    "googAutoGainControl": "false",
-                    "googNoiseSuppression": "false",
-                    "googHighpassFilter": "false"
-                },
-                "optional": []
-            },
-        }, gotStream, didntGetStream);
-    } catch (e) {
-        alert('getUserMedia threw exception :' + e);
-    }
+	// grab an audio context
+	audioContext = new AudioContext();
+
+	// Attempt to get audio input
+	try {
+		// monkeypatch getUserMedia
+		navigator.getUserMedia =
+			navigator.getUserMedia ||
+			navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia;
+
+		// ask for an audio input
+		navigator.getUserMedia(
+		{
+			"audio": {
+				"mandatory": {
+					"googEchoCancellation": "false",
+					"googAutoGainControl": "false",
+					"googNoiseSuppression": "false",
+					"googHighpassFilter": "false"
+				},
+				"optional": []
+			},
+		}, gotStream, didntGetStream);
+	} catch (e) {
+		alert('getUserMedia threw exception :' + e);
+	}
 
 }
 
 
 function didntGetStream() {
-    alert('Stream generation failed.');
+	alert('Stream generation failed.');
 }
 
 var mediaStreamSource = null;
 
 function gotStream(stream) {
-    // Create an AudioNode from the stream.
-    mediaStreamSource = audioContext.createMediaStreamSource(stream);
+	// Create an AudioNode from the stream.
+	mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
-    // Create a new volume meter and connect it.
-    meter = createAudioMeter(audioContext);
-    mediaStreamSource.connect(meter);
+	// Create a new volume meter and connect it.
+	meter = createAudioMeter(audioContext);
+	mediaStreamSource.connect(meter);
 
-    // kick off the visual updating
-    drawLoop();
+	// kick off the visual updating
+	drawLoop();
 }
 
 function drawLoop( time ) {
-    // clear the background
-    canvasContext.clearRect(0,0,WIDTH,HEIGHT);
+	// clear the background
+	canvasContext.clearRect(0,0,WIDTH,HEIGHT);
 
-    // check if we're currently clipping
-    if (meter.checkClipping())
-        canvasContext.fillStyle = "red";
-    else
-        canvasContext.fillStyle = "green";
+	// check if we're currently clipping
+	if (meter.checkClipping()) {
+		canvasContext.fillStyle = "red";
+		if (bellNotInterfering) {
+			bellNotInterfering = false;
+			bell.play();
+			setTimeout(function() {
+				bellNotInterfering = true;
+			}, 3500);
+		}
+	} else {
+		canvasContext.fillStyle = "green";
+	}
 
-    // draw a bar based on the current volume
-    canvasContext.fillRect(0, 0, meter.volume*WIDTH*1.4, HEIGHT);
+	// draw a bar based on the current volume
+	canvasContext.fillRect(0, 0, meter.volume*WIDTH*1.4, HEIGHT);
 
-    // set up the next visual callback
-    rafID = window.requestAnimationFrame( drawLoop );
+	// set up the next visual callback
+	rafID = window.requestAnimationFrame( drawLoop );
 }
